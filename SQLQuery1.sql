bede6cc90e06285e2a0e2f7f851afd50c17b6d36
Rambau Mukundi Eldridge <31636774+jackie20@users.noreply.github.com>
2023-06-22 20:38:14 +0200

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE TaxCalculations (
+    Id INT PRIMARY KEY IDENTITY,
+    PostalCode VARCHAR(20) NOT NULL,
+    AnnualIncome DECIMAL(18, 2) NOT NULL,
+    TaxAmount DECIMAL(18, 2) NOT NULL,
+    CalculationDate DATETIME NOT NULL
+);
